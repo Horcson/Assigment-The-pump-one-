b0390ea4822cb816689681adcf6d950daaa43a1d
Owen Warren <ow184@student.aru.ac.uk>
2022-10-26 21:49:08 +0100

    Do loop, Pump selection and if statements(9)

--- Program.cs
+++ Program.cs
@@ -4,62 +4,72 @@ string[] pump = new string[9] { "OPEN", "OPEN", "OPEN", "OPEN", "OPEN", "OPEN", 
 //UI thingy (Not finsished probably)
 Console.WriteLine("pump 1 " + pump[0] + " pump 2 " + pump[1] + " pump 3 " + pump[2] + " pump 4 " + pump[3] + " pump 5 " + pump[4] + " pump 6 " + pump[5] + " pump 7 " + pump[6] + " pump 8 " + pump[7] + " pump 9 " + pump[8]);
 
-int pumpSelect;
-Console.WriteLine("Select a pump");
-pumpSelect = Convert.ToInt32(Console.ReadLine());
 
-if (pumpSelect == 1)
+//pump selection stuff BROTHA
+int i = 0;
+do
 {
-    pump[0] = "OCCUPIED";
-    Console.WriteLine("pump 1 " + pump[0] + " pump 2 " + pump[1] + " pump 3 " + pump[2] + " pump 4 " + pump[3] + " pump 5 " + pump[4] + " pump 6 " + pump[5] + " pump 7 " + pump[6] + " pump 8 " + pump[7] + " pump 9 " + pump[8]);
-}
 
-if (pumpSelect == 2)
-{
-    pump[1] = "OCCUPIED";
-    Console.WriteLine("pump 1 " + pump[0] + " pump 2 " + pump[1] + " pump 3 " + pump[2] + " pump 4 " + pump[3] + " pump 5 " + pump[4] + " pump 6 " + pump[5] + " pump 7 " + pump[6] + " pump 8 " + pump[7] + " pump 9 " + pump[8]);
-}
+    int pumpSelect;
+    Console.WriteLine("Select a pump");
+    pumpSelect = Convert.ToInt32(Console.ReadLine());
+    //void userInput(string[] pump)
+    //{
+    if (pumpSelect == 1)
+    {
+        pump[0] = "OCCUPIED";
+        Console.WriteLine("pump 1 " + pump[0] + " pump 2 " + pump[1] + " pump 3 " + pump[2] + " pump 4 " + pump[3] + " pump 5 " + pump[4] + " pump 6 " + pump[5] + " pump 7 " + pump[6] + " pump 8 " + pump[7] + " pump 9 " + pump[8]);
+    }
 
-if (pumpSelect == 3)
-{
-    pump[2] = "OCCUPIED";
-    Console.WriteLine("pump 1 " + pump[0] + " pump 2 " + pump[1] + " pump 3 " + pump[2] + " pump 4 " + pump[3] + " pump 5 " + pump[4] + " pump 6 " + pump[5] + " pump 7 " + pump[6] + " pump 8 " + pump[7] + " pump 9 " + pump[8]);
-}
+    if (pumpSelect == 2)
+    {
+        pump[1] = "OCCUPIED";
+        Console.WriteLine("pump 1 " + pump[0] + " pump 2 " + pump[1] + " pump 3 " + pump[2] + " pump 4 " + pump[3] + " pump 5 " + pump[4] + " pump 6 " + pump[5] + " pump 7 " + pump[6] + " pump 8 " + pump[7] + " pump 9 " + pump[8]);
+    }
 
-if (pumpSelect == 4)
-{
-    pump[3] = "OCCUPIED";
-    Console.WriteLine("pump 1 " + pump[0] + " pump 2 " + pump[1] + " pump 3 " + pump[2] + " pump 4 " + pump[3] + " pump 5 " + pump[4] + " pump 6 " + pump[5] + " pump 7 " + pump[6] + " pump 8 " + pump[7] + " pump 9 " + pump[8]);
-}
+    if (pumpSelect == 3)
+    {
+        pump[2] = "OCCUPIED";
+        Console.WriteLine("pump 1 " + pump[0] + " pump 2 " + pump[1] + " pump 3 " + pump[2] + " pump 4 " + pump[3] + " pump 5 " + pump[4] + " pump 6 " + pump[5] + " pump 7 " + pump[6] + " pump 8 " + pump[7] + " pump 9 " + pump[8]);
+    }
 
-if (pumpSelect == 5)
-{
-    pump[4] = "OCCUPIED";
-    Console.WriteLine("pump 1 " + pump[0] + " pump 2 " + pump[1] + " pump 3 " + pump[2] + " pump 4 " + pump[3] + " pump 5 " + pump[4] + " pump 6 " + pump[5] + " pump 7 " + pump[6] + " pump 8 " + pump[7] + " pump 9 " + pump[8]);
-}
+    if (pumpSelect == 4)
+    {
+        pump[3] = "OCCUPIED";
+        Console.WriteLine("pump 1 " + pump[0] + " pump 2 " + pump[1] + " pump 3 " + pump[2] + " pump 4 " + pump[3] + " pump 5 " + pump[4] + " pump 6 " + pump[5] + " pump 7 " + pump[6] + " pump 8 " + pump[7] + " pump 9 " + pump[8]);
+    }
 
-if (pumpSelect == 6)
-{
-    pump[5] = "OCCUPIED";
-    Console.WriteLine("pump 1 " + pump[0] + " pump 2 " + pump[1] + " pump 3 " + pump[2] + " pump 4 " + pump[3] + " pump 5 " + pump[4] + " pump 6 " + pump[5] + " pump 7 " + pump[6] + " pump 8 " + pump[7] + " pump 9 " + pump[8]);
-}
+    if (pumpSelect == 5)
+    {
+        pump[4] = "OCCUPIED";
+        Console.WriteLine("pump 1 " + pump[0] + " pump 2 " + pump[1] + " pump 3 " + pump[2] + " pump 4 " + pump[3] + " pump 5 " + pump[4] + " pump 6 " + pump[5] + " pump 7 " + pump[6] + " pump 8 " + pump[7] + " pump 9 " + pump[8]);
+    }
 
-if (pumpSelect == 7)
-{
-    pump[6] = "OCCUPIED";
-    Console.WriteLine("pump 1 " + pump[0] + " pump 2 " + pump[1] + " pump 3 " + pump[2] + " pump 4 " + pump[3] + " pump 5 " + pump[4] + " pump 6 " + pump[5] + " pump 7 " + pump[6] + " pump 8 " + pump[7] + " pump 9 " + pump[8]);
-}
+    if (pumpSelect == 6)
+    {
+        pump[5] = "OCCUPIED";
+        Console.WriteLine("pump 1 " + pump[0] + " pump 2 " + pump[1] + " pump 3 " + pump[2] + " pump 4 " + pump[3] + " pump 5 " + pump[4] + " pump 6 " + pump[5] + " pump 7 " + pump[6] + " pump 8 " + pump[7] + " pump 9 " + pump[8]);
+    }
 
-if (pumpSelect == 8)
-{
-    pump[7] = "OCCUPIED";
-    Console.WriteLine("pump 1 " + pump[0] + " pump 2 " + pump[1] + " pump 3 " + pump[2] + " pump 4 " + pump[3] + " pump 5 " + pump[4] + " pump 6 " + pump[5] + " pump 7 " + pump[6] + " pump 8 " + pump[7] + " pump 9 " + pump[8]);
-}
+    if (pumpSelect == 7)
+    {
+        pump[6] = "OCCUPIED";
+        Console.WriteLine("pump 1 " + pump[0] + " pump 2 " + pump[1] + " pump 3 " + pump[2] + " pump 4 " + pump[3] + " pump 5 " + pump[4] + " pump 6 " + pump[5] + " pump 7 " + pump[6] + " pump 8 " + pump[7] + " pump 9 " + pump[8]);
+    }
 
-if (pumpSelect == 9)
-{
-    pump[8] = "OCCUPIED";
-    Console.WriteLine("pump 1 " + pump[0] + " pump 2 " + pump[1] + " pump 3 " + pump[2] + " pump 4 " + pump[3] + " pump 5 " + pump[4] + " pump 6 " + pump[5] + " pump 7 " + pump[6] + " pump 8 " + pump[7] + " pump 9 " + pump[8]);
+    if (pumpSelect == 8)
+    {
+        pump[7] = "OCCUPIED";
+        Console.WriteLine("pump 1 " + pump[0] + " pump 2 " + pump[1] + " pump 3 " + pump[2] + " pump 4 " + pump[3] + " pump 5 " + pump[4] + " pump 6 " + pump[5] + " pump 7 " + pump[6] + " pump 8 " + pump[7] + " pump 9 " + pump[8]);
+    }
+
+    if (pumpSelect == 9)
+    {
+        pump[8] = "OCCUPIED";
+        Console.WriteLine("pump 1 " + pump[0] + " pump 2 " + pump[1] + " pump 3 " + pump[2] + " pump 4 " + pump[3] + " pump 5 " + pump[4] + " pump 6 " + pump[5] + " pump 7 " + pump[6] + " pump 8 " + pump[7] + " pump 9 " + pump[8]);
+    }
 }
+while (i < 5);
+//}
 
 Console.ReadKey();
