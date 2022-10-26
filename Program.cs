447431adf35cb9e372af3def73cbaa4c44f08392
Owen Warren <ow184@student.aru.ac.uk>
2022-10-26 21:19:31 +0100

    add type

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
-﻿//Pump  String Array
+﻿//Pump String Array innit
 string[] pump = new string[9] {"OPEN", "OPEN", "OPEN", "OPEN", "OPEN", "OPEN", "OPEN", "OPEN", "OPEN"};
 
-//UI thing
-Console.WriteLine("pump 1 " + pump[1] + " pump 2 " + pump[2] + " pump 3 " + pump[3] + " pump 4 " + pump[2] + " pump 5 " + pump[3] + " pump 6 " + pump[2] + " pump 7 " + pump[3] + " pump 8 " + pump[8] + " pump 9 " + pump[0]);
+//UI thingy (Not finsished probably)
+Console.WriteLine("pump 1 " + pump[0] + " pump 2 " + pump[1] + " pump 3 " + pump[2] + " pump 4 " + pump[3] + " pump 5 " + pump[4] + " pump 6 " + pump[5] + " pump 7 " + pump[6] + " pump 8 " + pump[7] + " pump 9 " + pump[8]);
 
+type
 
 Console.ReadKey();
