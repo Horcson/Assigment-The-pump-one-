c4000f08568ec51227a2a626e01bf6fac9679866
Owen <ow184@student.aru.ac.uk>
2022-11-24 13:13:55 +0000

    Core criteria (I think)

--- Program.cs
+++ Program.cs
@@ -2,8 +2,10 @@
 
 //Pump String Array 
 string[] pump = new string[9] { "OPEN", "OPEN", "OPEN", "OPEN", "OPEN", "OPEN", "OPEN", "OPEN", "OPEN" };
-
+Timer pumpOpen = null;
 //Menu for the attendant to send cars from
+
+
 static void UI(string[] pump)
 {
 Console.WriteLine("pump 1: " + pump[0] + " |pump 2: " + pump[1] + " |pump 3: " + pump[2]);
@@ -16,7 +18,14 @@ Console.WriteLine("10: View fuel dispensed");
 Fuel Fuel = new Fuel();
 Cars cars = new Cars();
 cars.CarNum();
-
+void pumpO(object o)
+        {
+            //Adds the amount of fuel that has been used up during the 8 second fuel process
+            int pumpNo = Convert.ToInt32(o);
+            Fuel.addFuel();
+            pump[pumpNo] = "OPEN";
+            pumpOpen.Dispose();
+        } 
 //UI A
 UI(pump);
 
@@ -34,129 +43,95 @@ do
 
         //Displays the amount of cars in a queue
         cars.CarNum();
-        //Adds the amount of fuel that has been used up during the 8 second fuel process
-        Fuel.addFuel();
         //Pump selection menu
         UI(pump);
 
         //Timer that sets the pump to open after fueling process is
-        Timer pumpOpen = new Timer(pumpO, null, 8000, 8000);
-        void pumpO(object o)
-        {
-            pump[0] = "OPEN";
-        }
+        pumpOpen = new Timer(pumpO, pumpSelect, 8000, 8000);
+        
+     
+
+        
     }
 
     if (pumpSelect == 2)
     {
         pump[1] = "BUSY";
         cars.CarNum();
-        Fuel.addFuel();
         UI(pump);
         
-        Timer pumpOpen = new Timer(pumpO, null, 8000, 8000);
-        void pumpO(object o)
-        {
-            pump[1] = "OPEN";
-        }
+        pumpOpen = new Timer(pumpO, pumpSelect, 8000, 8000);
+        
     }
 
     if (pumpSelect == 3)
     {
         pump[2] = "BUSY";
         cars.CarNum();
-        Fuel.addFuel();
         UI(pump);
         
-        Timer pumpOpen = new Timer(pumpO, null, 8000, 8000);
-        void pumpO(object o)
-        {
-            pump[2] = "OPEN";
-        }
+        pumpOpen = new Timer(pumpO, pumpSelect, 8000, 8000);
+        
     }
 
     if (pumpSelect == 4)
     {
         pump[3] = "BUSY";
         cars.CarNum();
-        Fuel.addFuel();
         UI(pump);
 
-        Timer pumpOpen = new Timer(pumpO, null, 8000, 8000);
-        void pumpO(object o)
-        {
-            pump[3] = "OPEN";
-        }
+         pumpOpen = new Timer(pumpO, pumpSelect, 8000, 8000);
+        
     }
 
     if (pumpSelect == 5)
     {
         pump[4] = "BUSY";
         cars.CarNum();
-        Fuel.addFuel();
         UI(pump);
 
-        Timer pumpOpen = new Timer(pumpO, null, 8000, 8000);
-        void pumpO(object o)
-        {
-            pump[4] = "OPEN";
-        }
+         pumpOpen = new Timer(pumpO, pumpSelect, 8000, 8000);
+       
     }
 
     if (pumpSelect == 6)
     {
         pump[5] = "BUSY";
         cars.CarNum();
-        Fuel.addFuel();
         UI(pump);
 
-        Timer pumpOpen = new Timer(pumpO, null, 8000, 8000);
-        void pumpO(object o)
-        {
-            pump[5] = "OPEN";
-        }
+         pumpOpen = new Timer(pumpO, pumpSelect, 8000, 8000);
+        
     }
 
     if (pumpSelect == 7)
     {
         pump[6] = "BUSY";
         cars.CarNum();
-        Fuel.addFuel();
         UI(pump);
 
-        Timer pumpOpen = new Timer(pumpO, null, 8000, 8000);
-        void pumpO(object o)
-        {
-            pump[6] = "OPEN";
-        }
+         pumpOpen = new Timer(pumpO, pumpSelect, 8000, 8000);
+        
     }
 
     if (pumpSelect == 8)
     {
         pump[7] = "BUSY";
         cars.CarNum();
-        Fuel.addFuel();
         UI(pump);
 
-        Timer pumpOpen = new Timer(pumpO, null, 8000, 8000);
-        void pumpO(object o)
-        {
-            pump[7] = "OPEN";
-        }
+         pumpOpen = new Timer(pumpO, pumpSelect, 8000, 8000);
+        
     }
 
     if (pumpSelect == 9)
     {
         pump[8] = "BUSY";
         cars.CarNum();
-        Fuel.addFuel();
         UI(pump);
 
-        Timer pumpOpen = new Timer(pumpO, null, 8000, 8000);
-        void pumpO(object o)
-        {
-            pump[8] = "OPEN";
-        }
+         pumpOpen = new Timer(pumpO, pumpSelect, 8000, 8000);
+       
     }
     //Displays how much fuel has been dispensed
     if (pumpSelect == 10)
